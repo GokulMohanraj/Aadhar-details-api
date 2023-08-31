466628c6846903d7eeb5948a6801131b30a020a5
Gokul Mohanraj <thegokulraj@gmail.com>
2023-08-31 16:02:02 +0530

    Added role migration file

---
+++ migrations/20230831102914-create-roles.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable('roles', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      roleName: {
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable('roles');
+  }
+};
